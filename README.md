d813f4daf4f08547739728ff31b0ab1c6a40a43b
Amit Rungta <chehaktechnologies@gmail.com>
2020-05-01 15:03:04 +0530

    Added Login, Signup , logout option

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 Web Programming with Python and JavaScript
 
 
-In this 
+In this project we have created the following pages.
+
+1) Index Page : this page is used for creating a new account. This page will give the option to set the default settings for creating a new account. If there is any problem in the page then the user will be redirected to this page with the error message from the create account dialog.
+
--- application.py
+++ application.py
@@ -2,8 +2,7 @@ import os
 
 from flask import Flask,  session , render_template , redirect , url_for , escape , request
 from flask_session import Session
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import create_engine 
 
 app = Flask(__name__)
 # Configure session to use filesystem
@@ -24,36 +23,170 @@ else:
 
  # Set up database
 engine = create_engine(DatabaseUrl)
-db = scoped_session(sessionmaker(bind=engine))
+connection = engine.connect()
 
 
+
+class UserRegistrationData:
+    def __init__(self, DisplayName, UserName):
+        self.DisplayName = DisplayName
+        self.UserName = UserName
+
+
+# General method to convert string for Query format.
+def IsValidUserName( UserName , CanContainBlank = False):
+    if ( ( None == UserName ) or (not(UserName and UserName.strip())) ) :
+        return "cannot be empty."
+
+    if ( CanContainBlank == False and -1 != UserName.rfind (" ") ):
+        return "cannot contain blank spaces."
+
+    if ( -1 != UserName.rfind ("'") or \
+        -1 != UserName.rfind ('"') or \
+        -1 != UserName.rfind ("=") or \
+        -1 != UserName.rfind (";") or \
+        -1 != UserName.rfind ("=") or \
+        -1 != UserName.rfind ("*") or \
+        -1 != UserName.rfind ("%") or \
+        -1 != UserName.rfind ("/") or \
+        -1 != UserName.rfind ("\\") \
+        ):
+        return "cannot contain special character."
+    return ""
+
+
+
+
+
+#--------------------------------------------------------------------------
+# show the signup page...
+#
 @app.route("/")
 def index():
      return render_template("index.html")
-   # if 'username' in session:
-    #     username = session['username']
-    #     return 'Logged in as ' + username + '<br>' + \
-    #         "<b><a href = '/logout'>click here to log out</a></b>"
-    # return "You are not logged in <br><a href = '/login'></b>" + \
-    #     "click here to log in</b></a>"
 
 
+#--------------------------------------------------------------------------
+# will be called from response for createaccount button state. We should come here only from the post command.
+#
+@app.route('/createaccount', methods = ['GET', 'POST'])
+def createaccount():
+    if request.method == 'GET':
+        # if the user have come here directly then move back to index page.
+        return redirect(url_for('index'))
+
+    UserRegistrationData.UserName = request.form.get("inputUserName")
+    UserRegistrationData.DisplayName = request.form.get("inputDisplayName")
+
+    UserPassword = request.form.get("inputPassword")
+    UserRePassword = request.form.get("inputRePassword")
+
+    NameErrror = IsValidUserName(UserRegistrationData.DisplayName , True)
+    if ( None != NameErrror ) and (len(NameErrror) > 0 ) :
+        return  render_template("index.html", ErrorMsg=f"Display name {NameErrror}" , RegistrationData = UserRegistrationData)
+
+    NameErrror = IsValidUserName(UserRegistrationData.UserName)
+    if ( None != NameErrror ) and (len(NameErrror) > 0 ) :
+        return  render_template("index.html", ErrorMsg=f"User name {NameErrror}" , RegistrationData = UserRegistrationData)
+
+    NameErrror = IsValidUserName(UserPassword)
+    if ( None != NameErrror ) and (len(NameErrror) > 0 ) :
+        return  render_template("index.html", ErrorMsg=f"Password {NameErrror}" , RegistrationData = UserRegistrationData)
+
+    if ( ( None == UserRePassword ) or ( UserPassword != UserRePassword ) ) :
+        return  render_template("index.html", ErrorMsg="Retyped Password is not same as the original", RegistrationData = UserRegistrationData)
+
+
+    # now lets check if the user name is already present or not.
+    #  
+    SelectQuery = f"SELECT *FROM UserData WHERE UserName = '{UserRegistrationData.UserName}'"
+    RowCount = connection.execute(SelectQuery).rowcount
+    if ( RowCount > 0):
+        return  render_template("index.html", ErrorMsg="User with the given name already exist. Please select a different name.", RegistrationData = UserRegistrationData)
+
+    # we are here this menas we can add this new user.
+    InsertQuery = f"INSERT INTO UserData (UserName, Password , DisplayName ) VALUES ('{UserRegistrationData.UserName}', '{UserPassword}', '{UserRegistrationData.DisplayName}' )"
+    connection.execute(InsertQuery)
+
+    return redirect(url_for('signin'))
+
+
+
+
+
+#--------------------------------------------------------------------------
+# show the signin page...
+#
+@app.route('/signin')
+def signin():
+     return render_template("signin.html")
+
+
+
+#--------------------------------------------------------------------------
+# will be called from response for createaccount button state. We should come here only from the post command.
+#
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        session['username'] = request.form['username']
-        return redirect(url_for('index'))
-    return '''
-	
-   <form action = "" method = "post">
-        <input type="text" name="username" placeholder="Name..">
-        <input type="submit" value="Login">
-   </form>
-	
-   '''
+    if request.method == 'GET':
+        # if the user have come here directly then move back to signin page.
+        return redirect(url_for('signin'))
 
+    UserName = request.form.get("inputUserName")
+    UserPassword = request.form.get("inputPassword")
+
+    NameErrror = IsValidUserName(UserName)
+    if ( None != NameErrror ) and (len(NameErrror) > 0 ) :
+        return  render_template("signin.html", ErrorMsg=f"User name {NameErrror}" , UserName = UserName)
+
+    NameErrror = IsValidUserName(UserPassword)
+    if ( None != NameErrror ) and (len(NameErrror) > 0 ) :
+        return  render_template("signin.html", ErrorMsg=f"Password {NameErrror}" , UserName = UserName)
+
+    # now lets check if the user with giben name and password esit or not.
+    SelectData = None
+    try:
+        SelectData = connection.execute("SELECT User_ID, DisplayName FROM UserData WHERE UserName = %(username)s AND Password = %(password)s", {'username': UserName , 'password' : UserPassword}).fetchone()
+        if ( None == SelectData ) :
+            return  render_template("signin.html", ErrorMsg="User details for given name or password could not be fetched" , UserName = UserName)
+
+    except :
+        return  render_template("signin.html", ErrorMsg="Error in processing query" , UserName = UserName)
+
+    finally:
+        if ( None != SelectData ):
+            session['UserDisplayName'] = SelectData[1]
+            session['UserID'] = SelectData[0]
+            return  redirect(url_for('booksearch'))
+
+    return redirect(url_for('signin'))
+
+
+
+#--------------------------------------------------------------------------
+# Logout from the current session...
+#
 @app.route('/logout')
 def logout():
    # remove the username from the session if it is there
-   session.pop('username', None)
-   return redirect(url_for('index'))
+   session.pop('UserDisplayName', None)
+   session.pop('UserID', None)
+   return redirect(url_for('signin'))
+
+
+
+#--------------------------------------------------------------------------
+# show the book search page...
+#
+@app.route('/booksearch')
+def booksearch():
+   # Do the Book Search
+   UserID = session.get('UserID')
+   if ( None == UserID or len(UserID) < 1 ) :
+       return redirect(url_for('signin'))
+
+   return  render_template("booksearch.html", UserDisplayName=session.get('UserDisplayName'))
+
+
+
+
